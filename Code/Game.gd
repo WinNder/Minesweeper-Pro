864afded9fc7da9439a4e4bc91c179e56ee67c97
KrotMaster <jojojopos@mail.ru>
2023-02-05 02:17:13 +0300

    Исправление многочисленных багов, механика победы

--- Code/Game.gd
+++ Code/Game.gd
@@ -9,12 +9,13 @@ const FLAG = 1
 const EMPTY = 2
 const EMPTY_HIT = 3
 const NUMBER = 4
+const FLAG_RIGHT = 5
 var board = {}
 var mines = []
 var died = false
 var columns = 9
 var rows = 9
-var mines_count = 2
+var mines_count = 10
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
@@ -43,12 +44,14 @@ func _ready():
 				
 	#$chd.get_child(1).connect("pressed", self, "sweep")
 	var rng = RandomNumberGenerator.new()
-	for n in int(mines_count):
+	var n = 0
+	while n < mines_count:
 		rng.randomize()
 		var MineX = rng.randi_range(0,int(columns)-1)
 		var MineY = rng.randi_range(0,int(rows)-1)
 		if board[Vector2(MineX, MineY)] != MINE:	
 			board[Vector2(MineX, MineY)] = MINE
+			n+=1
 		#print(MineX, MineY)
 			print("Мина расположена на: ", MineX, " ", MineY)
 			#for b in (columns):
@@ -59,82 +62,45 @@ func _ready():
 		
 			
 		#if $chd.get_child((MineX+1)*(MineY+1)).is_connected("gui_input", self, "mine") == false:
-			$chd.get_child((columns*MineY)+MineX+1).connect("gui_input", self, "mine",[$chd.get_child((columns*MineY)+MineX+1)])
+			$chd.get_child((columns*MineY)+MineX+1).connect("gui_input", self, "mine",[$chd.get_child((columns*MineY)+MineX+1), MineX, MineY])
 			mines.append($chd.get_child((columns*MineY)+MineX+1))
 	pass # Replace with function body.
 	
-func mine(event, x):
-	#x.set_normal_texture(load("res://pplatebomb.png"))
-	if event is InputEventMouseButton and event.pressed:
+func mine(event, num, x, y):
+	if event is InputEventMouseButton and event.pressed and board[Vector2(x, y)] != FLAG_RIGHT and !died:
 		match event.button_index:
 			BUTTON_LEFT:
 				for mine in mines:
 					mine.set_normal_texture(load("res://pplatebomb.png"))
 				print("Game Over!")
 				died = true
-			BUTTON_RIGHT:
-				if !died:
-					x.set_normal_texture(load("res://plateflag.png"))
+				
 func click(event, btn, x, y):
 	#check()
 	if event is InputEventMouseButton and event.pressed:
 		match event.button_index:
 			BUTTON_LEFT:
-				var local_mines = 0
-				for i in columns-2:
-					for j in rows-2:
-						if Vector2(x-1+i,y-1+j) in board:
-							if board[Vector2(x-1+i, y-1+j)] == MINE:
-								local_mines += 1
-				if board[Vector2(x, y)] != EMPTY_HIT:
-					if local_mines > 0:
-						sweep($chd.get_child(columns*y+x+1), x, y)
-					elif local_mines == 0:
-						for a in columns-2:
-							for b in rows-2:
-								sweep($chd.get_child((columns*(y-1+b))+(x-1+a)+1), x+a-1, y+b-1);
-								print("Открываю x: ", x+a-1, " и y: ", y+b-1)
+				if board[Vector2(x, y)] != EMPTY_HIT and board[Vector2(x, y)] != FLAG and board[Vector2(x, y)] != FLAG_RIGHT:
+					sweep($chd.get_child(columns*y+x+1), x, y)
+					
 									#else:
 										#btn.set_normal_texture(load("res://pressedplate.png"))
 										#board[Vector2(i,j)] = EMPTY_HIT
-					
-#					if Vector2(x+1,y+1) in board:
-#						if board[Vector2(x+1, y+1)] == MINE:
-#							local_mines += 1
-#					if Vector2(x-1,y-1) in board: 
-#						if board[Vector2(x-1, y-1)] == MINE:
-#							local_mines += 1
-#					if Vector2(x-1,y+1) in board: 
-#						if board[Vector2(x-1, y+1)] == MINE:
-#							local_mines += 1
-#					if Vector2(x+1,y-1) in board:
-#						if board[Vector2(x+1, y-1)] == MINE: 
-#							local_mines += 1
-#					if Vector2(x,y+1) in board:
-#						if board[Vector2(x, y+1)] == MINE: 
-#							local_mines += 1
-#					if  Vector2(x,y-1) in board:
-#						if board[Vector2(x, y-1)] == MINE: 
-#							local_mines += 1
-#					if Vector2(x+1,y) in board:  
-#						if board[Vector2(x+1, y)] == MINE:  
-#							local_mines += 1
-#					if Vector2(x-1,y) in board:
-#						if board[Vector2(x-1, y)] == MINE:
-#							local_mines += 1
-					
-					
-					
 			BUTTON_RIGHT:
 				if !died: #and x.get_normal_texture() != load("res://pressedplate.png")
 					#print(board[Vector2(x,y)])
 					if Vector2(x,y) in board:
-						if board[Vector2(x,y)] != EMPTY and board[Vector2(x,y)] != EMPTY_HIT:
+						if board[Vector2(x,y)] == FLAG:
 							board[Vector2(x,y)] = EMPTY
 							btn.set_normal_texture(load("res://plate.png"))
-						elif board[Vector2(x,y)] != EMPTY_HIT:
+						elif board[Vector2(x,y)] == FLAG_RIGHT:
+							board[Vector2(x,y)] = MINE
+							btn.set_normal_texture(load("res://plate.png"))
+						elif board[Vector2(x,y)] == MINE:
+							board[Vector2(x,y)] = FLAG_RIGHT
+							btn.set_normal_texture(load("res://plateflag.png"))
+						elif board[Vector2(x,y)] == EMPTY:
 							board[Vector2(x,y)] = FLAG
-							
 							btn.set_normal_texture(load("res://plateflag.png"))
 						
 					#else:
@@ -149,19 +115,32 @@ func click(event, btn, x, y):
 	
 func sweep(btn, x, y):
 	if Vector2(x,y) in board:
-		if !died and board[Vector2(x,y)] != FLAG:
+		if !died and board[Vector2(x,y)] != FLAG and board[Vector2(x,y)] != FLAG_RIGHT:
 						var local_mines = 0
-						for i in 3:
-							for j in 3:
-								if Vector2(x-1+i,y-1+j) in board:
-									if board[Vector2(x-1+i, y-1+j)] == MINE: #or board[Vector2(x-1+i, y-1+j)] == FLAG:
+						for i in range(-1, 2):
+							for j in range (-1, 2):
+								if Vector2(x+i,y+j) in board:
+									if board[Vector2(x+i, y+j)] == MINE or board[Vector2(x+i,y+j)] == FLAG_RIGHT: #or board[Vector2(x-1+i, y-1+j)] == FLAG:
 										local_mines += 1
 						if local_mines > 0:
+							board[Vector2(x,y)] = NUMBER
 							btn.set_normal_texture(load("res://pplate_"+str(local_mines)+".png"))
 							#board[Vector2(x,y)] = NUMBER
 						else:
 							btn.set_normal_texture(load("res://pressedplate.png"))
 							board[Vector2(x,y)] = EMPTY_HIT
+							for a in range (-1, 2):
+								for b in range (-1, 2):
+									if Vector2(x+a,y+b) in board && board[Vector2(x+a, y+b)] != EMPTY_HIT:
+										sweep($chd.get_child((columns*(y+b))+(x+a)+1), x+a, y+b)
+						var check = true
+						for i in board:
+							if board[i] == FLAG or board[i] == EMPTY:
+									check = false
+						if check == true:
+							print("Victory")
+							died = true
+									
 #func check():
 #	var hits = 0
 #	for i in int(columns):
